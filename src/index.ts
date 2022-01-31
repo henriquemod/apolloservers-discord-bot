6cb61963e010c6d4a1449961f97d74cdf889463e
Henrique Souza <henrique.asouza@outlook.com>
2022-01-30 21:54:34 -0300

    chore: prod and __pwencription__ validation as well message when guild created

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,17 @@ import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 import { initializeWOK } from './utils/wokCommandsInit'
 import path from 'path'
-// import GuildSchema from './schemas/guild'
+import { __prod__, __pwencription__ } from './utils/constants'
 
-dotenv.config()
+if (!__prod__) {
+  dotenv.config()
+}
 
 const main = async (): Promise<void> => {
+  if (!__pwencription__) {
+    throw new Error('You must define a master key!!!')
+  }
+
   await mongoose.connect(process.env.MONGO_URI ?? '', {
     keepAlive: true
   })
@@ -16,14 +22,16 @@ const main = async (): Promise<void> => {
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
   })
 
+  client.on('guildCreate', async (guild) => {
+    await guild.systemChannel?.send({
+      content: "Thank's for adding me into your server"
+    })
+  })
+
   client.on('ready', () => {
     initializeWOK(client, path.join(__dirname, 'commands'))
     console.log('Bot is ready')
   })
-
-  // await new GuildSchema({
-  //   guildId: 'asd'
-  // }).save()
 
   await client.login(process.env.TOKEN)
 }
